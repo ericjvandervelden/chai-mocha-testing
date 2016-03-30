7c7ad3ee311d591e7314501d629c27ffb03d1f87
Eric J. Van der Velden <ericjvandervelden@gmail.com>
2016-03-30 21:50:56 +0200

    My first test with Chai Mocha

---
+++ test/test2.js
@@ -0,0 +1,7 @@
+
+var expect=require('chai').expect;
+describe('A basic test',function(){
+	it('it should pass this one also when everything is ok',function(){
+		expect(true).to.be.true;
+	});
+});
